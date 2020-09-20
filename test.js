d667cf163f0c1de49482ff6015a26d4d381b1ff8
Erman İlçin <ermanilcin@gmail.com>
2020-09-20 13:36:50 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,33 @@
-function SeatingStudents(arr) {
-  
-  const numOfSeats = arr.shift();
-  const numOfRows = Math.round(numOfSeats/2);
+function SeatingStudents(arr) { 
 
-  const seat = [];
+    const numOfSeats = arr.shift();
+    const numOfRows = Math.round(numOfSeats/2);
+    const seat = [];
 
-  let idx = 0;
-  for (row = 0; row < numOfRows; row ++) {
-    seat.push([]);
-    for (col = 0; col < 2; col ++) {
-      const isOccupied = arr.indexOf(idx + 1) > -1 ? true : false;
-      seat[row].push(isOccupied);
-      idx ++;
+    let idx = 0;
+    
+    for(row = 0; row < numOfRows; row++){
+
+      seat.push([]);
+
+      for(col = 0; col < 2; col++){
+        
+        const isOccupied = arr.indexOf(idx + 1) > -1 ? true : false; 
+        seat[row].push(isOccupied);
+        idx++;
+      }
     }
-    console.log(seat[row].join("-"));
-  }
-  
-  let seating = 0;
-  for (let i = 0; i < numOfRows - 1; i ++) {
-    if ((seat[i][0] === false) && (seat[i][1] === false)) seating ++;
-    if ((seat[i][0] === false) && (seat[i + 1][0] === false)) seating ++;
-    if ((seat[i][1] === false) && (seat[i + 1][1] === false)) seating ++;
+
+    let seating = 0;
+    for(let i=0; i < numOfRows -1; i++){
+      if ((seat[i][0] === false) && (seat[i][1] === false)) { seating++; }
+      if ((seat[i][0] === false) && (seat[i+1][0] === false)) { seating++; }
+      if ((seat[i][1] === false) && (seat[i+1][1] === false)) { seating++; }
+    }
+
+  if((seat[numOfRows - 1][0] == false) && (seat[numOfRows -1 ][1] == false)){
+    return seating;
   }
 
-  if ((seat[numOfRows - 1][0] == false) && (seat[numOfRows - 1][1] == false)) seating ++;
-  return seating;
+  return arr; 
 }
